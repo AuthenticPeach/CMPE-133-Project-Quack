499171db44ec47ec8116613dee3aefd558ef0ffa
JonathanQu1 <Jonathanqu2@gmail.com>
2024-12-08 23:06:38 -0800

    Signup Hotfix

--- client/pages/SignupPage/signup.js
+++ client/pages/SignupPage/signup.js
@@ -80,7 +80,7 @@ inputs.forEach((input) => {
         input.setCustomValidity('This field is invalid.');
     }
   });
-  
+});
 const passwordInput = document.getElementById('password');
 const passwordStrength = document.getElementById('password-strength');
 
